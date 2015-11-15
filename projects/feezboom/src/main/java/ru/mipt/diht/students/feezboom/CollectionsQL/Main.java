c0f5baa72d13444d32ccd568a009aa1ed066b8b3
Andrey <algebroid.2011@gmail.com>
2015-11-15 17:24:17 +0300

    Collections?

---
+++ projects/feezboom/src/main/java/ru/mipt/diht/students/feezboom/CollectionsQL/Main.java
@@ -0,0 +1,10 @@
+package ru.mipt.diht.students.feezboom.CollectionsQL;
+
+/**
+ * * Created by avk on 15.11.15.
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Collections!");
+    }
+}
