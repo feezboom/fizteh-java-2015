ce017ff947e7293786f28956dfe687b352ac18dd
Andrey <algebroid.2011@gmail.com>
2015-12-05 17:41:38 +0300

    collections

--- projects/feezboom/src/main/java/ru/mipt/diht/students/feezboom/CollectionsQL/Main.java
+++ projects/feezboom/src/main/java/ru/mipt/diht/students/feezboom/CollectionsQL/Main.java
@@ -17,31 +17,32 @@ import static ru.mipt.diht.students.feezboom.CollectionsQL.implementation.FromSt
 
 public class Main {
 
-    @SuppressWarnings("checkstyle:magicnumber")
-    public static void main(String[] args) {
-/*        Iterable<Statistics> statistics =
-                from(list(
-                        student("ivanov", LocalDate.parse("1986-08-06"), "494"),
-                        student("ivanov", LocalDate.parse("1986-08-06"), "494")))
-                        .select(Statistics.class, Student::getGroup, count(Student::getGroup), avg(Student::age))
-                        .where(rlike(Student::getName, ".*ov").and(s -> s.age() > 20))
-                        .groupBy(Student::getGroup)
-                        .having(s -> s.getCount() > 0)
-                        .orderBy(asc(Student::getGroup), desc(count(Student::getGroup)))
-                        .limit(100)
-                        .union()
-                        .from(list(student("ivanov", LocalDate.parse("1985-08-06"), "494")))
-                        .selectDistinct(Statistics.class, s -> "all", count(s -> 1), avg(Student::age))
-                        .execute();
-        System.out.println(statistics);
-*/      Iterable<Statistics> statistics =
-                from(list(
-                        student("ivanov", LocalDate.parse("1986-08-06"), "494"),
-                        student("ivanov", LocalDate.parse("1986-08-06"), "494")))
-                        .select(Statistics.class, Student::getGroup, count(Student::getGroup), avg(Student::age))
-                        .execute();
+@SuppressWarnings("checkstyle:magicnumber")
+public static void main(String[] args) {
+    Iterable<Statistics> statistics =
+        from(list(
+                student("ivanov", LocalDate.parse("1986-08-06"), "494"),
+                student("ivanov", LocalDate.parse("1986-08-06"), "494")))
+                .select(Statistics.class, Student::getGroup, count(Student::getGroup), avg(Student::age))
+                .where(rlike(Student::getName, ".*ov").and(s -> s.age() > 20))
+                .groupBy(Student::getGroup)
+                .having(s -> s.getCount() > 0)
+                .orderBy(asc(Student::getGroup), desc(count(Student::getGroup)))
+                .limit(100)
+                .union()
+                .from(list(student("ivanov", LocalDate.parse("1985-08-06"), "494")))
+                .selectDistinct(Statistics.class, s -> "all", count(s -> 1), avg(Student::age))
+                .execute();
+    System.out.println(statistics);
 
-    }
+    statistics =
+            from(list(
+                    student("ivanov", LocalDate.parse("1986-08-06"), "494"),
+                    student("ivanov", LocalDate.parse("1986-08-06"), "494")))
+                    .select(Statistics.class, Student::getGroup, count(Student::getGroup), avg(Student::age))
+                    .execute();
+
+}
 
 
     public static class Student {
